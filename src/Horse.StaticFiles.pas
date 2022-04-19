9db86b784e684feadae2c5eb1704b706d5b557c2
andre-djsystem <andre@djsystem.com.br>
2022-04-19 13:59:43 -0300

    Starting Lazarus compatibility

--- src/Horse.StaticFiles.pas
+++ src/Horse.StaticFiles.pas
@@ -1,25 +1,31 @@
 unit Horse.StaticFiles;
 
+
+{$IF DEFINED(FPC)}
+  {$MODE DELPHI}{$H+}
+{$ENDIF}
+
 interface
 
 uses
-
+  {$IF DEFINED(FPC)}
+  Generics.Collections,
+  Classes,
+  SysUtils,
+  {$ELSE}
   System.Generics.Collections,
   System.Classes,
   System.SysUtils,
+  {$ENDIF}
   Horse;
 
 type
 
+  { THorseStaticFileCallback }
+
   THorseStaticFileCallback = class
-  private
-    FPathRoot: string;
-    FDefaultFiles: TArray<string>;
   public
     class function New: THorseStaticFileCallback;
-    function SetPathRoot(APathRoot: string): THorseStaticFileCallback;
-    function SetDefaultFiles(ADefaultFiles: TArray<string>): THorseStaticFileCallback;
-    procedure Callback(AHorseRequest: THorseRequest; AHorseResponse: THorseResponse; ANext: TProc);
   end;
 
   THorseStaticFileManager = class
@@ -37,15 +43,24 @@ type
     class property DefaultManager: THorseStaticFileManager read GetDefaultManager;
   end;
 
-function HorseStaticFile(APathRoot: string; const ADefaultFiles: TArray<string> = []): THorseCallback; overload;
+function HorseStaticFile(APathRoot: string; const ADefaultFiles: TArray<string>): THorseCallback; overload;
+procedure Middleware(AHorseRequest: THorseRequest; AHorseResponse: THorseResponse; ANext: {$IF DEFINED(FPC)}TNextProc{$ELSE}TProc{$ENDIF});
 
 implementation
 
 uses
+  {$IF DEFINED(FPC)}
+   fpmimetypes;
+  {$ELSE}
   System.IOUtils,
   System.Net.Mime;
+  {$ENDIF}
 
-function HorseStaticFile(APathRoot: string; const ADefaultFiles: TArray<string> = []): THorseCallback; overload;
+var
+  PathRoot: string;
+  DefaultFiles: TArray<string>;
+
+function HorseStaticFile(APathRoot: string; const ADefaultFiles: TArray<string>): THorseCallback; overload;
 var
   LHorseStaticFileCallback: THorseStaticFileCallback;
 begin
@@ -56,71 +71,91 @@ begin
     .CallbackList
     .Add(LHorseStaticFileCallback);
 
-  Result :=
-    LHorseStaticFileCallback
-    .SetPathRoot(APathRoot)
-    .SetDefaultFiles(ADefaultFiles)
-    .Callback;
+  PathRoot := APathRoot;
+  DefaultFiles := ADefaultFiles;
+
+  Result := {$IF DEFINED(FPC)}@Middleware{$ELSE}Middleware{$ENDIF};
 end;
 
-{ THorseStaticFileCallback }
-
-procedure THorseStaticFileCallback.Callback(AHorseRequest: THorseRequest; AHorseResponse: THorseResponse; ANext: TProc);
+procedure Middleware(AHorseRequest: THorseRequest;
+  AHorseResponse: THorseResponse; ANext: TNextProc);
 var
   LFileStream: TFileStream;
   LNormalizeFileName: string;
+  {$IFNDEF FPC}
   LType: string;
   LKind: TMimeTypes.TKind;
+  {$ENDIF}
   I: Integer;
 begin
+  LNormalizeFileName := AHorseRequest.RawWebRequest.PathInfo.TrimLeft(['/']);
 
-  LNormalizeFileName := AHorseRequest.RawWebRequest.RawPathInfo.TrimLeft(['/']);
-
+  {$IFDEF FPC}
+  LNormalizeFileName := LNormalizeFileName.Replace('/', PathDelim);
+  LNormalizeFileName := ConcatPaths([PathRoot,LNormalizeFileName]);
+  {$ELSE}
   LNormalizeFileName := LNormalizeFileName.Replace('/', TPath.DirectorySeparatorChar);
+  LNormalizeFileName := TPath.Combine(PathRoot, LNormalizeFileName);
+  {$ENDIF}
 
-  LNormalizeFileName := TPath.Combine(FPathRoot, LNormalizeFileName);
-
-  if (TDirectory.Exists(LNormalizeFileName)) or (ExtractFileName(LNormalizeFileName).IsEmpty) then
+  {$IFDEF FPC}
+  if DirectoryExists(LNormalizeFileName) or (Trim(ExtractFileName(LNormalizeFileName)) = EmptyStr)  then
   begin
-    for I := Low(FDefaultFiles) to High(FDefaultFiles) do
+    for I := Low(DefaultFiles) to High(DefaultFiles) do
     begin
-      if TFile.Exists(TPath.Combine(LNormalizeFileName, FDefaultFiles[I])) then
+      if FileExists(ConcatPaths([LNormalizeFileName, DefaultFiles[I]])) then
       begin
-        LNormalizeFileName := TPath.Combine(LNormalizeFileName, FDefaultFiles[I]);
+        LNormalizeFileName := ConcatPaths([LNormalizeFileName, DefaultFiles[I]]);
         Break;
       end;
     end;
   end;
+  {$ELSE}
+  if (TDirectory.Exists(LNormalizeFileName)) or (ExtractFileName(LNormalizeFileName).IsEmpty) then
+  begin
+    for I := Low(DefaultFiles) to High(DefaultFiles) do
+    begin
+      if TFile.Exists(TPath.Combine(LNormalizeFileName, DefaultFiles[I])) then
+      begin
+        LNormalizeFileName := TPath.Combine(LNormalizeFileName, DefaultFiles[I]);
+        Break;
+      end;
+    end;
+  end;
+  {$ENDIF}
 
+  {$IFDEF FPC}
+  if FileExists(LNormalizeFileName) then
+  {$ELSE}
   if TFile.Exists(LNormalizeFileName) then
+  {$ENDIF}
   begin
     LFileStream := TFileStream.Create(LNormalizeFileName, fmShareDenyNone or fmOpenRead);
-    AHorseResponse.RawWebResponse.ContentStream := LFileStream;
-    TMimeTypes.Default.GetFileInfo(LNormalizeFileName, LType, LKind);
-    AHorseResponse.RawWebResponse.ContentType := LType;
-    AHorseResponse.RawWebResponse.StatusCode := 200;
-    AHorseResponse.RawWebResponse.SendResponse;
-    raise EHorseCallbackInterrupted.Create;
+    try
+      AHorseResponse.RawWebResponse.ContentStream := LFileStream;
+
+      {$IFDEF FPC}
+      MimeTypes.LoadKnownTypes;
+      AHorseResponse.ContentType(MimeTypes.GetMimeType(ExtractFileExt(LNormalizeFileName)));
+      {$ELSE}
+      TMimeTypes.Default.GetFileInfo(LNormalizeFileName, LType, LKind);
+      AHorseResponse.RawWebResponse.ContentType := LType;
+      {$ENDIF}
+      AHorseResponse.Status(THTTPStatus.OK);
+      AHorseResponse.RawWebResponse.SendResponse;
+      raise EHorseCallbackInterrupted.Create;
+    finally
+      LFileStream.Free;
+    end;
   end;
 
   ANext();
 end;
 
+{ THorseStaticFileCallback }
 class function THorseStaticFileCallback.New: THorseStaticFileCallback;
 begin
-  Result := THorseStaticFileCallback.Create;
-end;
-
-function THorseStaticFileCallback.SetPathRoot(APathRoot: string): THorseStaticFileCallback;
-begin
-  Result := Self;
-  FPathRoot := APathRoot;
-end;
-
-function THorseStaticFileCallback.SetDefaultFiles(ADefaultFiles: TArray<string>): THorseStaticFileCallback;
-begin
-  Result := Self;
-  FDefaultFiles := ADefaultFiles;
+  Result := Self.Create;
 end;
 
 { THorseStaticFileManager }
